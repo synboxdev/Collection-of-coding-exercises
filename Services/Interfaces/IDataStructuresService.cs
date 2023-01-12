6466a67fde514e9945a3c0ac8156e9f8fee0f3fb
synboxdev <synbox.dev@gmail.com>
2023-01-12 20:40:45 +0200

    Exercise "Remove duplicate node elements from a given Linked List" - 2 Solutions and 4 Unit test (Two per solution)

--- Services/Interfaces/IDataStructuresService.cs
+++ Services/Interfaces/IDataStructuresService.cs
@@ -5,4 +5,6 @@ public interface IDataStructuresService
     public LinkedList<int> ReverseALinkedList(LinkedList<int> linkedList);
     public LinkedListNode<int>? FindNodeInLinkedList(LinkedList<int>? linkedList, int? valueToFind);
     public LinkedList<int> MergeTwoLinkedListsUsingLINQ(LinkedList<int> linkedListOne, LinkedList<int> linkedListTwo);
+    public LinkedList<int> RemoveDuplicatesFromLinkedListUsingHashSet(LinkedList<int> linkedList);
+    public LinkedList<int> RemoveDuplicatesFromLinkedListUsingLINQ(LinkedList<int> linkedList);
 }
