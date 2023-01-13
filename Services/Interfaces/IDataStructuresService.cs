00a7b53ccf1eb7bab17a25a4ba24c6bd50650a58
synboxdev <synbox.dev@gmail.com>
2023-01-13 18:42:55 +0200

    Exercise "Reverse a given Stack collection" - 1 Solution and 2 Unit tests. Also changed ConsoleOutput method, in Data Structures service, to accept a generic IEnumerable collection.

--- Services/Interfaces/IDataStructuresService.cs
+++ Services/Interfaces/IDataStructuresService.cs
@@ -7,4 +7,5 @@ public interface IDataStructuresService
     public LinkedList<int> MergeTwoLinkedListsUsingLINQ(LinkedList<int> linkedListOne, LinkedList<int> linkedListTwo);
     public LinkedList<int> RemoveDuplicatesFromLinkedListUsingHashSet(LinkedList<int> linkedList);
     public LinkedList<int> RemoveDuplicatesFromLinkedListUsingLINQ(LinkedList<int> linkedList);
+    public Stack<string> ReverseAStack(Stack<string> inputStack);
 }
--- Services/Services/DataStructuresService.cs
+++ Services/Services/DataStructuresService.cs
@@ -9,18 +9,18 @@ public class DataStructuresService : IDataStructuresService
     /// A very simple generic type helper method, to reduce bloat of each solution method.
     /// This method simply prints out a given generic type LinkedList, either inline (all nodes in same console line) or new line for each node element. Also takes case of spacing of output data.
     /// </summary>
-    private void ConsoleOutputLinkedList<T>(ConsoleOutputType outputType, LinkedList<T> linkedList)
+    private void ConsoleOutputEnumerableCollection<T>(ConsoleOutputType outputType, IEnumerable<T> collectionToPrint)
     {
         if (outputType == ConsoleOutputType.Inline)
         {
-            for (int i = 0; i < linkedList.Count; i++)
-                Console.Write($"{linkedList.ElementAt(i)}  ");
+            for (int i = 0; i < collectionToPrint.Count(); i++)
+                Console.Write($"{collectionToPrint.ElementAt(i)}  ");
             Console.WriteLine();
         }
         if (outputType == ConsoleOutputType.NewLine)
         {
-            for (int i = 0; i < linkedList.Count; i++)
-                Console.WriteLine($"{linkedList.ElementAt(i)}");
+            for (int i = 0; i < collectionToPrint.Count(); i++)
+                Console.WriteLine($"{collectionToPrint.ElementAt(i)}");
         }
     }
 
@@ -34,7 +34,7 @@ public class DataStructuresService : IDataStructuresService
                 linkedList.AddLast(i);
         }
         Console.WriteLine("Here are nodes of our input Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedList);
 
         // Initialize a new, empty Linked List, which we will populate with values and form a reversed version of our input linked list.
         LinkedList<int> reversedLinkedList = new LinkedList<int>();
@@ -49,7 +49,7 @@ public class DataStructuresService : IDataStructuresService
             startNode = startNode.Previous;
         }
         Console.WriteLine("Here are nodes of our reversed Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, reversedLinkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, reversedLinkedList);
 
         return reversedLinkedList;
     }
@@ -68,7 +68,7 @@ public class DataStructuresService : IDataStructuresService
                 linkedList.AddLast(i);
         }
         Console.WriteLine("Here are nodes of our input Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedList);
 
         // We utilize LinkedList's function Find that returns a LinkedListNode with a given value. If no such node if found - null is returned.
         var foundNode = linkedList.Find((int)valueToFind);
@@ -87,7 +87,7 @@ public class DataStructuresService : IDataStructuresService
                 linkedListOne.AddLast(Random.Shared.Next(1, 20));
         }
         Console.WriteLine("Here are nodes of our first Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedListOne);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedListOne);
 
         if (linkedListTwo == null || linkedListTwo.Count == 0)
         {
@@ -96,12 +96,12 @@ public class DataStructuresService : IDataStructuresService
                 linkedListTwo.AddLast(Random.Shared.Next(1, 20));
         }
         Console.WriteLine("Here are nodes of our second Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedListTwo);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedListTwo);
 
         // Probably the most simple and elegant solution to this problem - is to utilize LINQ's Concat (Which is SQL's equivalent of UnionAll) and OrderBy functions
         LinkedList<int> mergedLinkedList = new LinkedList<int>(linkedListOne.Concat(linkedListTwo).OrderBy(x => x));
         Console.WriteLine("Here's a final, merged and sorted Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, mergedLinkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, mergedLinkedList);
 
         return mergedLinkedList;
     }
@@ -119,7 +119,7 @@ public class DataStructuresService : IDataStructuresService
             linkedList.AddLast(3);
         }
         Console.WriteLine("Here are nodes of our first Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedList);
 
         // Instantiate a new HashSet, iterate over every node in the input Linked List. Only unique elements will be added to HashSet, because if such element it NOT yet present inside the HashSet.
         HashSet<int> hashSetOfNodes = new HashSet<int>();
@@ -134,7 +134,7 @@ public class DataStructuresService : IDataStructuresService
             linkedList.AddLast(hashSetNode);
 
         Console.WriteLine("Here's a final Linked List, without any duplicate values:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedList);
         return linkedList;
     }
 
@@ -151,7 +151,7 @@ public class DataStructuresService : IDataStructuresService
             linkedList.AddLast(3);
         }
         Console.WriteLine("Here are nodes of our first Linked List:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedList);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedList);
 
         // Instantiate a new LinkedList of the same type as our input Linked List;
         LinkedList<int> linkedListWithoutDuplicates = new LinkedList<int>();
@@ -163,7 +163,29 @@ public class DataStructuresService : IDataStructuresService
         }
 
         Console.WriteLine("Here's a final Linked List, without any duplicate values:");
-        ConsoleOutputLinkedList(ConsoleOutputType.Inline, linkedListWithoutDuplicates);
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, linkedListWithoutDuplicates);
         return linkedListWithoutDuplicates;
+    }
+
+    public Stack<string> ReverseAStack(Stack<string> inputStack)
+    {
+        // If our provided Stack as a parameter, is null or does not have any nodes (values) inside it - we create our own Stack.
+        if (inputStack == null || inputStack.Count == 0)
+        {
+            string[] randomElements = new string[] { "Apple", "Orange", "Banana", "Pear" };
+            inputStack = new Stack<string>(randomElements);
+        }
+        Console.WriteLine("Here are nodes of our input Stack of strings:");
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, inputStack);
+
+        // Define a new Stack collection, into which we'll stored elements in efforts of reversing the input Stack.
+        Stack<string> reversedStack = new Stack<string>();
+        while (inputStack.Count > 0)                // While the input Stack collection is not empty, proceed with the iteration
+            reversedStack.Push(inputStack.Pop());   // Push functions Inserts a given object at the top of the Stack, while Pop function removes a given object from the top of the stack.
+                                                    // So we're placing the topmost element from the input Stack, into the reversed Stack.
+
+        Console.WriteLine("Here's reversed Stack and its elements:");
+        ConsoleOutputEnumerableCollection(ConsoleOutputType.Inline, reversedStack);
+        return reversedStack;
     }
 }
