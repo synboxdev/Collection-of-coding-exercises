76cc0ed2d00613eecfb8ec7ebcc76a21c300a2bc
synboxdev <synbox.dev@gmail.com>
2023-02-20 19:17:23 +0200

    Exercise "Find the largest prime factor of the number 600851475143" - 1 Solution and 1 Unit test

--- Services/Interfaces/IEulerService.cs
+++ Services/Interfaces/IEulerService.cs
@@ -5,4 +5,5 @@ public interface IEulerService
     public int MultiplesOf3And5();
     public int MultiplesOf3And5UsingLINQ();
     public int EvenFibonacciNumbers();
+    public int LargestPrimeFactor();
 }
--- Services/Services/EulerService.cs
+++ Services/Services/EulerService.cs
@@ -11,6 +11,12 @@ namespace Services;
 /// </summary>
 public class EulerService : IEulerService
 {
+    private readonly INumbersService _numbersService;
+
+    public EulerService(INumbersService numbersService)
+    {
+        _numbersService = numbersService;
+    }
 
     /// <summary>
     /// Find the sum of all the multiples of 3 or 5 below 1000.
@@ -76,5 +82,44 @@ public class EulerService : IEulerService
         int totalSum = fibonacciSequence.Sum();                 // Our total sum will simple be equal to the sum of all elements in the list. Since we've already ensued that it only contains even-valued Fibonacci sequence elements, whose values do not exceed four million.
         Console.WriteLine($"Sum of even-valued Fibonacci sequence elements, whose values do not exceed four million is equal to {totalSum}");
         return totalSum;
+    }
+
+    /// <summary>
+    /// Find the largest prime factor of the number 600851475143
+    /// Read more here: https://projecteuler.net/problem=3
+    /// </summary>
+    public int LargestPrimeFactor()
+    {
+        // Define the variable in question.
+        double inputNumber = 600851475143;
+        Console.WriteLine($"We will be finding the largest prime factor of the number {inputNumber}");
+
+        // Initialize a variable that will hold the largest prime factor for our number.
+        var largestPrimeFactor = 0;
+        // Also, initialize a range of values, that are rounded-up square roots of our input number. Reason for this is to reduce the number of total iterations required.
+        var range = Enumerable.Range(1, (int)Math.Round(Math.Sqrt(inputNumber) + 1));
+
+        // Iterate over every element in out range (defined above)
+        foreach (var element in range)
+        {
+            // Create a temporary number that will either hold the value of a factor or will simply remain null.
+            int? factor = inputNumber % element == 0 ? element : null;
+
+            // Assign a value to our 'largest prime factor' variable under the following three conditions:
+            // 1. Our factor has to have a value (not be null).
+            // 2. Our factor has to be a prime number (Call a method from previously solved exercise in 'Numbers' category)
+            // 3. And lastly - our factor must be a higher value than the previously saved value. (Since we're looking for the largest prime factor)
+            // If the condition above are met - new value for our 'largest prime factor' variable is assigned. 
+            largestPrimeFactor = (int)(factor != null &&
+                                       _numbersService.CheckIfNumberIsPrime(factor, true) &&
+                                       factor > largestPrimeFactor ?
+                                       factor : largestPrimeFactor);
+        }
+
+        // Once the loop has finished, display the results to the console window
+        Console.WriteLine($"{(largestPrimeFactor > 0 ?
+                          $"Largest prime factor for number {inputNumber} is equal to {largestPrimeFactor}" :
+                          $"Number {inputNumber} does NOT even have a single valid prime factor")}");
+        return largestPrimeFactor;
     }
 }
